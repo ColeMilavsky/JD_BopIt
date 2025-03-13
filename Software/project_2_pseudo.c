b0bf17f88eb9013ca9f4cf4cab5ba16b183674e2
ColeMilavsky <COM76@pitt.edu>
2025-03-13 11:39:07 -0400

    Proposal Files Included

---
+++ Software/project_2_pseudo.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main()
+{
+    /* INITIALIZATION CODE
+    Function to initialize microcontroller
+    {
+        Set up signals to transmit via SPI
+        {
+            Initialize HEX display
+            Initialize LEDs throughout enclosure
+        }
+        Set up signals to receive via SPI
+        {
+            Rotary Sensor Data
+            Microphone Data
+            Pressure Sensor Data
+        }
+    }
+    */
+    
+    /* GAME CODE
+    While(Game is being played)
+    {
+        if(task 1[incline] called)
+        {
+            while(timeForTask > timer)
+            {
+                Check rotary sensor for completed task
+
+                Update Hex
+                or
+                Play error sound, end game
+            }
+        }
+
+        if(task 2[stadium] called)
+        {
+            while(timeForTask > timer)
+            {
+                Check microphone for completed task
+
+                Update Hex
+                or
+                Play error sound, end game
+            }
+        }
+
+        if(task 3[bottle] called)
+        {
+            while(timeForTask > timer)
+            {
+                Check pressure sensor for completed task
+
+                Update Hex
+                or
+                Play error sound, end game
+            }
+        }
+    }
+    */      
+
+    return 0;
+}
